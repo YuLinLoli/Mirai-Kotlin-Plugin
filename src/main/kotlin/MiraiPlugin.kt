47bb0928a5475fc30de1f103d76ed013008d01b6
岚雨凛 <cheng_ying@outlook.com>
2023-03-19 14:09:56 +0800

    1.0.0 Mirai-Kotlin基本脚手架
    Signed-off-by: cheng_ying@outlook.com <cheng_ying@outlook.com>

--- src/main/kotlin/MiraiPlugin.kt
+++ src/main/kotlin/MiraiPlugin.kt
@@ -10,8 +10,8 @@ import net.mamoe.mirai.event.globalEventChannel
 
 object MiraiPlugin : KotlinPlugin(
     JvmPluginDescription(
-        id = "com.mirai.plugin",
-        name = "管理员命令",
+        id = "com.mirai.plugin（记得更改此处的插件id以及各种关于插件id的配置）",
+        name = "插件名在这里写！！",
         version = "1.0.0",
     )
 ) {
