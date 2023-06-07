daf2a78b5d100400749836cfeb5ff66a65168135
cheng_ying@outlook.com <cheng_ying@outlook.com>
2023-06-07 12:05:13 +0800

    1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.8.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.14.0"
+    id("net.mamoe.mirai-console") version "2.15.0-M1"
 }
 
 group = "com.mirai"
 version = "1.0.0"
 
 repositories {
-//    mavenLocal()
-//    maven("https://maven.aliyun.com/repository/gradle-plugin")
-//    maven("https://maven.aliyun.com/repository/central")
+    mavenLocal()
+    maven("https://maven.aliyun.com/repository/gradle-plugin")
+    maven("https://maven.aliyun.com/repository/central")
     mavenCentral()
 }
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.jsoup:jsoup:1.15.4")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("net.mamoe:mirai-core-jvm:2.14.0")
+    implementation("net.mamoe:mirai-core-jvm:2.15.0-M1")
     implementation("top.jfunc.common:converter:1.8.0")
 
 
